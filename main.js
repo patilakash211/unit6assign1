8f81a69e7029267bdbd4734185a96b0b0f633d9c
Akash Patil <patilakash211@gmail.com>
2022-04-18 02:02:06 +0530

    assignment1

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = require('./add');
+const app1 = require('./subtract');
+const app2 = require('./multiply');
+const app3 = require('./divide');
+
+console.log(app.add(6,6));
+console.log(app1.subtract(6,6));
+console.log(app2.multiply(6,6));
+console.log(app3.divide(6,6));
